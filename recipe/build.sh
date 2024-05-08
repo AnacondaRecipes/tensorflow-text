c347920cfcf1982b6b803cca198cf8a93f479409
Jack Olivieri <jolivieri@anaconda.com>
2024-05-08 22:53:38 +0200

    add useful comment

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,5 @@
+# adding host environment bin to the path because on some platforms it uses _build_env/bin/python
+# instead of the host environment python
 export PATH=$PREFIX/bin:$PATH
 
 ./oss_scripts/run_build.sh
